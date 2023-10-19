dcfe7a6d5af1cbc3f61e8a37147953b1170f51c5
Ravi Kumar Siddojigari <quic_rsiddoji@quicinc.com>
2023-10-19 21:07:53 +0530

    QcomModulePkg: remove root=/dev/dm-0 from cmdline for NAND device
    As the kernel code is still not ready for handling  mounting of
    /dev/dm-0 for NAND removing this as boot is going to wait
    for this node to be available.
    
    Change-Id: I81499565bc027471ee20c2bd3adbe02c0a06c2d9

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -551,17 +551,9 @@ GetSystemPath (CHAR8 **SysPath, BOOLEAN MultiSlotBoot, BOOLEAN BootIntoRecovery,
          " rootfstype=ubifs rootflags=bulk_read root=ubi0:rootfs%s ubi.mtd=%d",
                  CurSlot.Suffix, (Index - 1));
       } else {
-        if (IsLEVerity () &&
-          !BootIntoRecovery ) {
-           AsciiSPrint (*SysPath, MAX_PATH_SIZE,
-           " rootfstype=ext4  rootflags=bluk_read ubi.mtd=%d ubi.block=0,0 root=/dev/dm-0",
-            (Index - 1));
-          }
-        else {
           AsciiSPrint (*SysPath, MAX_PATH_SIZE,
           " rootfstype=ubifs rootflags=bulk_read root=ubi0:rootfs ubi.mtd=%d",
           (Index - 1));
-        }
       }
     }
   } else if (!AsciiStrCmp ("UFS", RootDevStr)) {
